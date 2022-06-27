507f3adfffda3bf6d7108762aa1f56a681f2aa2f
maxmudovilyos75@gmail.com <maxmudovilyos75@gmail.com>
2022-06-27 12:22:04 +0500

    Initial Commit

---
+++ FootZone/FootZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootZoneApp.swift
+//  FootZone
+//
+//  Created by iMtRo on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FootZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
